8970087a18e3f5368b6fc264c74ed4fd6d4c024d
Richard Budek <richardbudek@Richards-MacBook-Air.local>
2018-03-08 09:28:29 -0600

    customer working
    might want to make the quit make more sense by prompting for item-id first, then if quit, to quit

--- Bamazon-Customer/bamazonCustomer.js
+++ Bamazon-Customer/bamazonCustomer.js
@@ -5,7 +5,9 @@
 var mySQL = require('mysql');
 var inquirer = require("inquirer");
 var Table = require('cli-table');
+var numeral = require('numeral');
 
+//var numeral = new Numeral();
 
 //variable for connection
 var sqlConnection = mySQL.createConnection({
@@ -30,28 +32,43 @@ sqlConnection.connect(function (err) {
 //function display all products
 var dispAllProducts_start = function () {
     //no input, just blast out the files
-    console.log("start the query");
     sqlConnection.query("SELECT * FROM bamazon_db.products", function (error, data) {
         dispAllProducts_found(error, data);
     });
-    console.log("after query");
 };
 
 
 //products were found by the query
 var dispAllProducts_found = function (error, data) {
     //first check if there was an error
-    console.log("query done, in the found routine");
     var table = new Table({
-        head: ["Item", "Product name", "Department", "Price", "Qty in Stock"],
-        colWidths: [10, 40, 20, 10, 10]
+        head: ["Item", "Product name", "Department", "Price"],
+        colWidths: [15, 42, 20, 15]
     });
     if (error) throw error;
     var endVal = data.length;
     for (var i = 0; i < endVal; i++) {
-        table.push( [ data[i].item_id, data[i].product_name, data[i].department_name, data[i].price, data[i].stock_quantity ] );
+        var priceFormatted = numeral(data[i].price).format("$000,000.00");
+        table.push([data[i].item_id, data[i].product_name, data[i].department_name, priceFormatted]);
     };
-    console.log( table.toString() );
+    console.log(table.toString());
+    userPrompt();
+};
+
+
+//searches for item_id in the db and return true
+var validItemNum = function (_itemId) {
+    sqlConnection.query("SELECT * FROM bamazon_db.products", function (error, data) {
+        dispAllProducts_found(error, data);
+    });
+
+};
+
+
+//function to return the quantity of an item
+//will return quantity in stock
+var quantityInStock = function (_itemId) {
+
 };
 
 //db_update ( item_id, new qty )
@@ -69,8 +86,118 @@ var dispAllProducts_found = function (error, data) {
 //  -- what ID
 //  -- how many
 
+var userSelectObj = {
+    //global variable to hold what the user picked
+    //allows all of the callbacks to reach these values
+    item_id: "",
+    product_name: "",
+    department_name: "",
+    price: 0.0,
+    qty_in_stock: 0,
+    qty_to_buy: 0,
+    qty_after_buy: 0,
+    order_total: 0
+};
+
+
+var dispError = function (msgStr) {
+    console.log("");
+    console.log("------ ERROR -----------------");
+    console.log(msgStr);
+    console.log("------------------------------");
+    console.log("");
+};
+
+
+var dispOrder = function () {
+    console.log("");
+    console.log("---------your order -----------");
+    console.log(userSelectObj.item_id + "  " + userSelectObj.product_name );
+    var priceFormatted = numeral(userSelectObj.price).format("$000,000.00");
+    userSelectObj.order_total = userSelectObj.price * userSelectObj.qty_to_buy;
+    var totalFormatted = numeral( userSelectObj.order_total ).format("$0000,000.00");
+    console.log("unit price = " + priceFormatted + " x " + userSelectObj.qty_to_buy + " units = " + totalFormatted + " total" );
+    console.log("------------------------------");
+    console.log("\nIs shipping. thank-you\n\n");
+};
+
+
+var storeDBtoObj = function (_data) {
+    //store the data from the db to the global obj
+    userSelectObj.product_name = _data[0].product_name;
+    userSelectObj.department_name = _data[0].department_name;
+    userSelectObj.qty_in_stock = _data[0].stock_quantity;
+    userSelectObj.price = _data[0].price;
+};
+
+
+function userPrompt() {
+    inquirer.prompt([
+        {
+            name: "item_to_buy",
+            message: "Item to buy (quit to end): "
+        }, {
+            name: "quantity",
+            message: "How many to buy: "
+        }
+    ]).then(function (answers) {
+        //what the user picked  
+console.log("user selected");        
+        userSelectObj.item_id = answers.item_to_buy.trim().toUpperCase();
+        userSelectObj.qty_to_buy = parseInt(answers.quantity);
+        if (userSelectObj.item_id === "QUIT") {
+            sqlConnection.end();
+            return;
+        };
+console.log("searching");
+        sqlConnection.query("SELECT * FROM bamazon_db.products WHERE ?",
+            [{
+                item_id: userSelectObj.item_id
+            }],
+            function (error, data) {
+                //check if there were any hits
+                if (data.length <= 0) {
+                    dispError("bad item.  choose again");
+                    return start();
+                };
+                //there was a find, check if there is enough qty
+                storeDBtoObj(data);
+                userSelectObj.qty_after_buy = userSelectObj.qty_in_stock - userSelectObj.qty_to_buy;
+console.log(userSelectObj);                
+                if (userSelectObj.qty_after_buy < 0) {
+                    var msgStr = "Sorry, we are out of stock for your full order. \n";
+                    msgStr += "You are ordering " + userSelectObj.qty_to_buy + " items of " + userSelectObj.product_name + "\n";
+                    msgStr += "and we only have " + userSelectObj.qty_in_stock + ".";
+                    dispError(msgStr);
+                    return start();
+                } else {
+                    //so the quantity is correct update the database
+                    sqlConnection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: userSelectObj.qty_after_buy
+                            },
+                            {
+                                item_id: userSelectObj.item_id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw error;
+                            dispOrder();
+                            return start();
+                        }
+                    );
+                };
+            });
+    });
+};
+
+
 
 function start() {
     //start of program
     dispAllProducts_start();
+
 };
+
